a0f7d8547fbdde1a38ccadd126a0780df1a0ad77
ajm1379 <183442291+ajm1379@users.noreply.github.com>
2025-02-08 21:38:44 -0600

    initial commit i guess

--- rotorstorm-embedded.ino
+++ rotorstorm-embedded.ino
@@ -1,8 +1,27 @@
+alright this marks the begninning of libraires i might use
+
+BMP:
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+#define SEALEVELPRESSURE_HPA (1013.25)
+end of BMP with note that sealevelpressure is not right unit and use I2C not SPI NO SPIb
+
+INA:
+#include "Arduino.h"
+#include <Wire.h>
+#include "Adafruit_INA260.h"
+
 void setup() {
   // put your setup code here, to run once:
   //test
-place to stoe libraries
-muy computor trash
+
 }
 
 void loop() {
