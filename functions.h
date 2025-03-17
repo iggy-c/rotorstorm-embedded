c6efd74b072c625d35e807216928ee4a899ecc76
ajm1379 <183442291+ajm1379@users.noreply.github.com>
2025-03-17 16:38:26 -0500

    update
    no clue

--- functions.h
+++ functions.h
@@ -1,47 +1,47 @@
 void send_xbee(String message) {
-    uint8_t payloadLength = message.length();
-    uint8_t frame[20 + payloadLength];  
-    uint8_t frameLength = 14 + payloadLength; 
-    uint8_t checksum = 0;
+  uint8_t payloadLength = message.length();
+  uint8_t frame[20 + payloadLength];  
+  uint8_t frameLength = 14 + payloadLength; 
+  uint8_t checksum = 0;
 
-    frame[0] = 0x7E; //start delimiter
-    frame[1] = (frameLength >> 8) & 0xFF; //length
-    frame[2] = frameLength & 0xFF; //length
-    frame[3] = 0x10; //frame type
-    frame[4] = 0x01; //frame id
+  frame[0] = 0x7E; //start delimiter
+  frame[1] = (frameLength >> 8) & 0xFF; //length
+  frame[2] = frameLength & 0xFF; //length
+  frame[3] = 0x10; //frame type
+  frame[4] = 0x01; //frame id
 
-    frame[5]  = 0x00; //64-bit destination address
-    frame[6]  = 0x13;  
-    frame[7]  = 0xA2;  
-    frame[8]  = 0x00;  
-    frame[9]  = 0x42;  
-    frame[10] = 0x5B;  
-    frame[11] = 0xD6;  
-    frame[12] = 0x18;  
+  frame[5]  = 0x00; //64-bit destination address
+  frame[6]  = 0x13;  
+  frame[7]  = 0xA2;  
+  frame[8]  = 0x00;  
+  frame[9]  = 0x42;  
+  frame[10] = 0x5B;  
+  frame[11] = 0xD6;  
+  frame[12] = 0x18;  
 
-    frame[13] = 0xFF; //16-bit destination address
-    frame[14] = 0xFE;
+  frame[13] = 0xFF; //16-bit destination address
+  frame[14] = 0xFE;
 
-    frame[15] = 0x00; //broadcast radius
-    frame[16] = 0x00; //options
+  frame[15] = 0x00; //broadcast radius
+  frame[16] = 0x00; //options
 
-    // Copy the message into the payload
-    for (uint8_t i = 0; i < payloadLength; i++) {
-        frame[17 + i] = message[i];
-        checksum += message[i];
-    }
+  // Copy the message into the payload
+  for (uint8_t i = 0; i < payloadLength; i++) {
+      frame[17 + i] = message[i];
+      checksum += message[i];
+  }
 
-    checksum += 0x10 + 0x01;
-    checksum += 0x00 + 0x13 + 0xA2 + 0x00 + 0x42 + 0x5B + 0xD6 + 0x18;
-    checksum += 0xFF + 0xFE + 0x00 + 0x00;
-    checksum = 0xFF - checksum;
+  checksum += 0x10 + 0x01;
+  checksum += 0x00 + 0x13 + 0xA2 + 0x00 + 0x42 + 0x5B + 0xD6 + 0x18;
+  checksum += 0xFF + 0xFE + 0x00 + 0x00;
+  checksum = 0xFF - checksum;
 
-    frame[17 + payloadLength] = checksum;
+  frame[17 + payloadLength] = checksum;
 
-    Serial1.write(frame, 18 + payloadLength);
-    
-    Serial.print("XBee API frame sent:"); 
-    Serial.println(message);
+  Serial1.write(frame, 18 + payloadLength);
+  
+  Serial.print("XBee API frame sent:"); 
+  Serial.println(message);
 }
 
 void setReports(Adafruit_BNO08x bno08x) {
--- rotorstorm-embedded.ino
+++ rotorstorm-embedded.ino
@@ -5,14 +5,18 @@
 #include <Adafruit_BNO08x.h> //enables BNO
 #include <SD.h> // enables sd communication
 #include <Adafruit_INA260.h> // enables INA
+#include <SparkFun_u-blox_GNSS_v3.h>
 #include <ctime> // time library
 #include <Servo.h>
-
+// #include "Timer.h"
 #include "functions.h"
+// #include "time.h"
+// #include <arduino-timer.h>
 
 #define BMP390_ADDRESS 0x77  // Try 0x76 if this fails
 #define BNO08x_ADDRESS 0x4A // or 0x4B if DI pin is pulled high to VCC
 #define INA260_ADDRESS 0x40 // my shins hurt
+#define gnssAddress 0x42 // The default I2C address for u-blox modules is 0x42. Change this if required
 
 #define servo_wire 11 
 #define BMP_SDA 14 //SDA for I2C stuff for bmp
@@ -21,26 +25,66 @@
 #define BNO08X_SCL 15 //SCL for I2C stuff for bno
 #define INA260_SDA 14
 #define INA260_SCL 15
+#define GNSS_SDA 14
+#define GNSS_SCL 15
 
 #define SEALEVELPRESSURE_HPA (1013.25) 
 
+// Timer timer;
 Adafruit_BMP3XX bmp;
 Adafruit_BNO08x bno08x;
 Adafruit_INA260 ina260;
+SFE_UBLOX_GNSS myGNSS;
 Servo release_servo;
 
 const int UARTRX = 1;
 const int UARTTX = 0;
 
 int packetCount = 0;
-String echoStr = "";
+String echoStr = "None";
 int servoPos = 0;
 sh2_SensorValue_t sensorValue; // does something to help bno work
 
+// auto timer = timer_create_default();
+
+// Timer<16, millis, const char *> t_timer;
+
+// bool sendpacketandusetimer(void *)
+// {
+// String message = (
+//     "3194," //TEAM_ID
+//   + String(1) + "," //MISSION_TIME
+//   + String(packetCount) + "," //PACKET_COUNT
+//   + "mode,state," //MODESTATE
+//   + String(bmp.readAltitude(SEALEVELPRESSURE_HPA)) + "," //ALTITUDE
+//   + bmp.readTemperature() + "," //TEMPERATURE
+//   + bmp.readPressure() + "," //PRESSURE
+//   + String(ina260.readBusVoltage() / 1000) + "," //VOLTAGE
+//   + String(sensorValue.un.rawGyroscope.x) + "," 
+//   + String(sensorValue.un.rawGyroscope.y) + ","
+//   + String(sensorValue.un.rawGyroscope.z) + ","
+//   + String(sensorValue.un.linearAcceleration.x) + ","
+//   + String(sensorValue.un.linearAcceleration.y) + ","
+//   + String(sensorValue.un.linearAcceleration.z) + ","
+//   + "1,2,3,"
+//   + "rotate_rate,"
+//   + String(timer.read()) + ","
+//   + String(myGNSS.getAltitudeMSL()) + ","
+//   + String(myGNSS.getLatitude()) + ","
+//   + String(myGNSS.getLongitude()) + ","
+//   + "gpssats,echo"
+//   );
+
+//   packetCount++;
+//   return true;
+// }
+
+long lastTime = 0;
 
 void setup() {
 
   Serial.begin(9600);
+  // timer.start();
 
   // initialize XBee
   Serial1.setRX(UARTRX);
@@ -60,6 +104,11 @@ void setup() {
   // Initialize I2C1 (Wire1) for INA
   Wire1.setSDA(INA260_SDA);
   Wire1.setSCL(INA260_SCL);
+  Wire1.begin();
+
+  // Initialize I2C1 (Wire1) for GNSS
+  Wire1.setSDA(GNSS_SDA);
+  Wire1.setSCL(GNSS_SCL);
   Wire1.begin();
 
   Serial.println("Starting XBee API packet transmission...");
@@ -100,6 +149,12 @@ void setup() {
   }
   Serial.println("Found INA260 chip");
 
+  while (myGNSS.begin(Wire1, gnssAddress) == false) //Connect to the u-blox module using our custom port and address
+  {
+    Serial.println(F("u-blox GNSS not detected. Retrying..."));
+    delay (1000);
+  }
+
   // Set up oversampling and filter initialization
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
@@ -110,11 +165,24 @@ void setup() {
 
   release_servo.attach(servo_wire, 400, 2600);
   release_servo.write(60);
+
+  //Timer setup
+  // timer.every(1000, sendpacketandusetimer);
 }
 
 void loop() {
+
+// timer.tick(); // tick the timer
+
+  if (!bno08x.getSensorEvent(&sensorValue)) {
+    setReports(bno08x);
+    return;
+  }
+
+  // Serial.println("timer: ");
+  // Serial.println(timer.read());
   servoPos = 0;
-  String sp = "";
+  String str = "";
   int unused = 0;
 
   if (!bmp.performReading()) {
@@ -128,8 +196,63 @@ void loop() {
       delay(100);  // Allow time for reinitialization
   }
 
-  if (!bno08x.getSensorEvent(&sensorValue)) {
-    return;
+
+  if (myGNSS.getPVT() == true)
+  {
+    int32_t latitude = myGNSS.getLatitude();
+    Serial.print(F("Lat: "));
+    Serial.print(latitude);
+
+    int32_t longitude = myGNSS.getLongitude();
+    Serial.print(F(" Long: "));
+    Serial.print(longitude);
+    Serial.print(F(" (degrees * 10^-7)"));
+
+    int32_t altitude = myGNSS.getAltitudeMSL(); // Altitude above Mean Sea Level
+    Serial.print(F(" Alt: "));
+    Serial.print(altitude);
+    Serial.print(F(" (mm)"));
+
+    Serial.println();
+  }
+//beginning of section to start getting time
+  if (millis() - lastTime > 1000)
+  {
+    lastTime = millis(); //Update the timer
+
+    long latitude = myGNSS.getLatitude();
+    Serial.print(F("Lat: "));
+    Serial.print(latitude);
+
+    long longitude = myGNSS.getLongitude();
+    Serial.print(F(" Long: "));
+    Serial.print(longitude);
+    Serial.print(F(" (degrees * 10^-7)"));
+
+    long altitude = myGNSS.getAltitude();
+    Serial.print(F(" Alt: "));
+    Serial.print(altitude);
+    Serial.print(F(" (mm)"));
+
+    byte SIV = myGNSS.getSIV();
+    Serial.print(F(" SIV: "));
+    Serial.print(SIV);
+
+    
+    Serial.println();
+    // Serial.print(myGNSS.getYear());
+    // Serial.print("-");
+    // Serial.print(myGNSS.getMonth());
+    // Serial.print("-");
+    // Serial.print(myGNSS.getDay());
+    Serial.print(" ");
+    Serial.print(myGNSS.getHour());
+    Serial.print(":");
+    Serial.print(myGNSS.getMinute());
+    Serial.print(":");
+    Serial.print(myGNSS.getSecond());
+    Serial.print(" ");
+
   }
 
   /*
@@ -153,8 +276,14 @@ void loop() {
   + String(sensorValue.un.linearAcceleration.x) + ","
   + String(sensorValue.un.linearAcceleration.y) + ","
   + String(sensorValue.un.linearAcceleration.z) + ","
-  + "1,2,3,rotate_rate,gpstime,gpsaltitude,1,2,gpssats,echoz"
-  // + "1,2,3,rotate_rate,gpstime,gpsaltitude,1,2,gpssats,echoajf;lsak;jfd;lsaf;lsakjf;lsajfa;ljf;alksf;alsjf;alsjf;aljf;asljf;alskdjf;aljf;aljf;alskdjfa;lskdjf;aljf;aljf;alkjf;aljf;alkjfds;alksdjf;alskdjf"
+  + "1,2,3,"
+  + "rotate_rate,"
+  + String("placementfortime") + ","
+  + String(myGNSS.getAltitudeMSL()) + ","
+  + String(myGNSS.getLatitude()) + ","
+  + String(myGNSS.getLongitude()) + ","
+  + "gpssats" + ","
+  + String(echoStr)
   );
 
   packetCount++;
@@ -164,9 +293,10 @@ void loop() {
   while(Serial1.available()){
     int first_byte = Serial1.read();
     if (first_byte == 126) {
-      int length = Serial1.read() << 8 & 0xFF;
-      length += Serial1.read() & 0xFF;
-
+      // int length = Serial1.read() >> 8 & 0xFF;
+      Serial.println(Serial1.read());
+      int length = Serial1.read();
+      Serial.println(length);
       int packetType = Serial1.read();
  
       if (packetType == 139) { //If packet type is Transmit Status (0x8B)
@@ -179,13 +309,36 @@ void loop() {
       else if (packetType == 144) { //If packet type is Receive Packet (0x90)
         Serial.println("something received");
         for (int i = 0; i < 11; i++){
-          unused = Serial1.read();
+          // unused = Serial1.read();
+          Serial.print(char(Serial1.read()));
+          Serial.print(" ");
         }
-        for(int i = 0; i <= Serial1.available(); i++){
-          sp += char(Serial1.read());
+        Serial.println("\na;lskdfhja;dlfkj");
+        delay(1000);
+        for(int i = 0; i <= Serial1.available(); i++) {
+          Serial.println(char(Serial1.read()));
+          // str += char(Serial1.read());
+
         }
-        servoPos = sp.toInt();
-        release_servo.write(servoPos);
+        Serial.println("jhg ");
+        // Serial.println(str);
+        if (str.substring(0,3).equals("MEC")) {
+          Serial.print("2 ");
+          if (str.substring( str.indexOf(',') + 1, str.indexOf(',') + 3).equals("RE")) {
+            Serial.print("3 ");
+            if (str.substring( str.lastIndexOf(',') + 1, str.lastIndexOf(',' + 3) ).equals("ON")) {
+              Serial.print("4 ");
+              release_servo.write(60);
+            }
+            else if (str.substring( str.lastIndexOf(',') + 1, str.lastIndexOf(',' + 3) ).equals("OF")) {
+              Serial.print("5 ");
+              release_servo.write(85);
+            }
+          }
+        }
+        echoStr = str;
+        // servoPos = str.toInt();
+        // release_servo.write(servoPos);
         unused = Serial1.read();
       }
     }
