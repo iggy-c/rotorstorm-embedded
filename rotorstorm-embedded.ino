31099be5ff9dff81e04e0643785a35c93de7a992
iggy-c <113953538+iggy-c@users.noreply.github.com>
2025-06-07 05:48:45 -0400

    updated sim mode, flight modes, commands, switched xbee to transparent, fixed i2c bug, added time

--- rotorstorm-embedded.ino
+++ rotorstorm-embedded.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>                                  //enables I2C
-#include <SparkFun_u-blox_GNSS_Arduino_Library.h>  // enables GNSS
+#include <SparkFun_u-blox_GNSS_v3.h>  // enables GNSS
 #include "Adafruit_BMP3XX.h"  //enables BMP
 #include <Adafruit_Sensor.h>  //enables adafruit sensors
 #include <Servo.h>            // enables servo
 #include <Adafruit_BNO055.h> //enables BNO055
 #include <utility/imumaths.h>
 #include <arduino-timer.h>
+
 // #include "functions.h"
 
+uint32_t interval = 1000;
+#define MAX_PAYLOAD 100
 
 #define BMP390_ADDRESS 0x77  // Try 0x76 if this fails
 #define gnssAddress 0x42     // The default I2C address for u-blox modules is 0x42. Change this if required
@@ -24,20 +27,33 @@
 #define TACH_SDA 14
 #define TACH_SCL 15
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-#define START_DELIMITER 0x7E  // XBee API start byte
+#define SEALEVELPRESSURE_PA (101325.)
 
+
+bool telemetry = true;
+bool DEBUG_MODE = false;
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100; 
+bool using_bno055 = true;
+
+
+auto timer = timer_create_default();
 Servo release_servo;
 SFE_UBLOX_GNSS myGNSS;
 Adafruit_BMP3XX bmp;
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);  // go from wire to wire as needed
-auto timer = timer_create_default();
+Adafruit_BNO055 bno055 = Adafruit_BNO055(55, 0x28);  
 
-bool telemetry = true;
 int packetCount = 1;
-uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;  // this could cause timing issues
-String packet = "adsf";
+String packet = "";
 String echo;
+String buffer = "";
+String command = "";
+String mission_time = "";
+bool sentApo = false;
+String flightState = "LAUNCH_PAD";
+String flightMode = "F";
+bool sim_enable = false;
+float sim_last_commanded_altitude;
+
 
 float calibrated_pressure;
 int32_t latitude;
@@ -46,9 +62,6 @@ int32_t gps_altitude;
 uint16_t rpm;
 byte SIV;
 
-bool DEBUG_MODE = false;
-bool sentApo = false;
-String flightState = "LAUNCH_PAD";
 
 
 long lastTime = 0;  //Simple local timer. Limits amount if I2C traffic to u-blox module.
@@ -64,7 +77,7 @@ bool probe_release = false;
 
 
 float maxaltitude = 0;
- float currentaltitude = 0;
+float currentaltitude = 0;
 float previousaltitude = 0;
 float ppreviousaltitude = 0;
 float pppreviousaltitude = 0;
@@ -75,8 +88,7 @@ float bmaltitude = 0;
 
 sensors_event_t linearAccelData, magnetometerData, accelerometerData;
 
-
-bool send(void *) {
+bool send_api(void *) {
   uint8_t payloadLength = packet.length();
   uint8_t frame[20 + payloadLength];
   uint8_t frameLength = 14 + payloadLength;
@@ -119,7 +131,7 @@ bool send(void *) {
   checksum = 0xFF - checksum;
 
   frame[17 + payloadLength] = checksum;
-
+  
   Serial1.write(frame, 18 + payloadLength);
 
   Serial.print("XBee API frame sent: ");
@@ -128,6 +140,29 @@ bool send(void *) {
   return true;
 }
 
+bool send(void *) {
+  if(telemetry){
+    Serial1.print("[" + packet + "]");
+    Serial.print("sent packet: ");
+    Serial.println(packet);
+    packetCount++;
+  }
+  return true;
+
+}
+
+String cur_time(){
+  unsigned long runMillis = millis();
+  unsigned long allSeconds = millis()/1000;
+  int h = allSeconds / 3600;
+  int secsRemaining = allSeconds % 3600;
+  int m = secsRemaining / 60;
+  int s = secsRemaining % 60;
+  int ms = runMillis % 100;
+  char time[5];
+  sprintf(time, "%02d:%02d", m, s);
+  return String(time);
+}
 
 void setup() {
 
@@ -136,8 +171,9 @@ void setup() {
   Serial1.setRX(SER1_RX);
   Serial1.setTX(SER1_TX);
   Serial1.begin(9600);
-  while (DEBUG_MODE && !Serial)
-    ;
+  while (DEBUG_MODE && !Serial);
+
+  Wire.setTimeout(25, true);
 
   Serial.println("Serial Started");
   delay(100);
@@ -155,14 +191,15 @@ void setup() {
   if (!bmp.begin_I2C(0x76)) { 
     if (!bmp.begin_I2C(0x77)) { 
       Serial.println("Could not find a valid BMP3XX sensor, check wiring!");
-      while (DEBUG_MODE)
-        ; 
+      while (DEBUG_MODE); 
     }
   }
-  if (!bno.begin()) {
+  if (!bno055.begin()) {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
   }
+
+  bno055.setExtCrystalUse(true);
   // Checking BNO
   // if (myIMU.begin(BNO08X_ADDR, Wire, BNO08X_INT, BNO08X_RST) == false) {
   //     Serial.println("BNO08x not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
@@ -196,123 +233,134 @@ void setup() {
   //beginning of GNSS setup
   delay(100);
 
-  // if (myGNSS.begin(Wire, gnssAddress) == false) {
-  //   Serial.println(F("u-blox GNSS not detected"));
-  //   while(DEBUG_MODE){
-  //     ;
-  //   }
-  // }
+  if (myGNSS.begin(Wire, gnssAddress) == false) {
+    Serial.println(F("u-blox GNSS not detected"));
+    while(DEBUG_MODE){;
+    }
+  }
+
+  myGNSS.setNavigationFrequency(4); //Produce two solutions per second
+  myGNSS.setAutoPVT(true);
 
   release_servo.attach(SERVO_CTRL, 400, 2600);
   release_servo.write(80);
 
   delay(100);
 
-  timer.every(1000, send);
-  timer.every(1000, incr);
-}
+  timer.every(985, send);
 
-bool incr(void *){
-  packetCount++;
-  return true;
 }
 
 void loop() {
-
   timer.tick();
 
-  sensors_event_t linearAccelData, magnetometerData, accelerometerData;
-  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  if (Wire.getTimeoutFlag()){
+    Serial.println("Wire Error Flagged");
+    Wire.clearTimeoutFlag();
+  }
 
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
+  while(Serial1.available()){
+    buffer += char(Serial1.read());
+  }
+  if (buffer.indexOf("{") > -1 && buffer.indexOf("}") > -1){
+    command = buffer.substring(buffer.indexOf("{") + 1, buffer.indexOf("}"));
+    Serial.print("cmd received: ");
+    Serial.println(command);
+    buffer = buffer.substring(buffer.indexOf("}") + 1);
+
+    echo = command;
+    echo.replace(",","");
+
+    if (command.equals("CMD,3194,CAL")){
+      //todo
+    }
+    else if (command.equals("CMD,3194,MEC,RELEASE,OFF")){
+      release_servo.write(100);
+    }
+    else if (command.equals("CMD,3194,MEC,RELEASE,ON")){
+      release_servo.write(80);
+    }
+    else if (command.equals("CMD,3194,CX,ON")){
+      telemetry = true;
+    }
+    else if (command.equals("CMD,3194,CX,OFF")){
+      telemetry = false;
+    }
+    else if (command.equals("CMD,3194,ST,GPS")){
+      mission_time = String(myGNSS.getHour()) + ":" + String(myGNSS.getMinute()) + ":" + String(myGNSS.getSecond());
+    }
+    else if (command.substring(0,12).equals("CMD,3194,ST,")){
+      mission_time = command.substring(12);
+    }
+    else if (command.equals("CMD,3194,SIM,ENABLE")){
+      sim_enable = true;
+    }
+    else if (command.equals("CMD,3194,SIM,ACTIVATE")){
+      Serial.println("asdfasf");
+      if (sim_enable){
+        flightMode = "S";
+      }
+      sim_enable = false;
+    }
+    else if (command.equals("CMD,3194,SIM,DISABLE")){
+      flightMode = "F";
+      sim_enable = false;
+
+      bmaltitude = 0;
+      maximumaltitude = 0;
+      pppppreviousaltitude = 0;
+      ppppreviousaltitude = 0;
+      pppreviousaltitude = 0;
+      ppreviousaltitude = 0;
+      previousaltitude = 0;
+    }
+    else if (command.substring(0,13).equals("CMD,3194,SIMP") && flightMode.equals("S")){
+      sim_last_commanded_altitude = 44330.0 * (1.0 - pow((command.substring(command.lastIndexOf(",") + 1).toFloat()) / SEALEVELPRESSURE_PA, 0.1903));
+      Serial.print("SIMP: ");
+      Serial.println(sim_last_commanded_altitude);
+
+      bmaltitude = 0;
+      maximumaltitude = 0;
+      pppppreviousaltitude = 0;
+      ppppreviousaltitude = 0;
+      pppreviousaltitude = 0;
+      ppreviousaltitude = 0;
+      previousaltitude = 0;
+    }
+  }
+  // if(using_bno055){
+    sensors_event_t event;
+    bno055.getEvent(&event);
+
+    sensors_event_t linearAccelData, magnetometerData, accelerometerData;
+    bno055.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+    bno055.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+    bno055.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    // bno055.getCalibration(&system, &gyro, &accel, &mag);
+    // uint8_t system, gyro, accel, mag = 0;
+  // }
 
 
-  String packet_payload = "";
-  int unused = 0;
 
   if (!bmp.performReading()) {
     Serial.println("Failed to perform reading on BMP :(");
   }
 
 
-  // beginning of GNSS stuff
-  // if (myGNSS.getPVT() == true) {
-  //   latitude = myGNSS.getLatitude();
-  //   // Serial.print(myGNSS.getLatitude());
-  //   longitude = myGNSS.getLongitude();
-  //   // Serial.print(myGNSS.getLongitude());
-  //   gps_altitude = myGNSS.getAltitudeMSL();  // Altitude above Mean Sea Level
-  //   // Serial.println(gps_altitude);
 
-  //   lastTime = millis();  //Update the timer
-  //   SIV = myGNSS.getSIV();
-  //   // Serial.print(SIV);
-  //   Serial.println("end of gnss");
-  // }
-
-
-  //Handle received messages from XBee
-  while (Serial1.available()) {
-    int first_byte = Serial1.read();
-    if (first_byte == 126) {
-      int length = Serial1.read() << 8 & 0xFF;
-      length += Serial1.read() & 0xFF;
-
-      int packetType = Serial1.read();
-
-      if (packetType == 139) {  //If packet type is Transmit Status (0x8B)
-        for (int i = 0; i < length; i++) {
-          unused = Serial1.read();
-        }
-        Serial.println("Packet received by ground station");
-        // Serial.print(unused);
-      }
-
-      else if (packetType == 144) {  //If packet type is Receive Packet (0x90)
-        Serial.println("something received");
-        for (int i = 0; i < 11; i++) {
-          unused = Serial1.read();
-        }
-        for (int i = 0; i <= Serial1.available(); i++) {
-          packet_payload += char(Serial1.read());
-        }
-        if (packet_payload.equals("cal")) {
-          Serial.println("calibrating");
-          calibrated_pressure = bmp.pressure / 100;
-          echo = "CAL";
-        } else if (packet_payload.equals("rel")) {
-          Serial.println("releasing");
-          release_servo.write(65);
-          echo = "MECSERVOOFF"; // CanSat is "off" the rocket body
-        } else if (packet_payload.equals("clo")) {
-          Serial.println("priming");
-          release_servo.write(80);
-          echo = "MECSERVOON"; // CanSat is "on" the rocket body
-        } else if (packet_payload.equals("con")) {
-          Serial.println("telemetry on");
-          telemetry = true;
-          echo = "CXON";
-        } else if (packet_payload.equals("cof")) {
-          Serial.println("telemetry off");
-          telemetry = false;
-          echo = "CXOFF";
-        } else {
-          Serial.print("unrecognized command: ");
-          Serial.println(packet_payload);
-        }
-
-        unused = Serial1.read();
-      }
-    }
+  if (myGNSS.getPVT() == true) {
+    latitude = myGNSS.getLatitude();
+    longitude = myGNSS.getLongitude();
+    gps_altitude = myGNSS.getAltitudeMSL();  // Altitude above Mean Sea Level
+    SIV = myGNSS.getSIV();
   }
-  // Serial.println(packet_payload);
-  Serial1.flush();
 
-
-  currentaltitude = bmp.readAltitude(calibrated_pressure);
+  if (flightMode.equals("F")){
+    currentaltitude = bmp.readAltitude(calibrated_pressure);
+  }
+  else if (flightMode.equals("S")){
+    currentaltitude = sim_last_commanded_altitude;
+  }
 
   flightState = statemachine(currentaltitude, previousaltitude, ppreviousaltitude, pppreviousaltitude, ppppreviousaltitude, pppppreviousaltitude, maximumaltitude, bmaltitude);
   bmaltitude = maximumaltitude;
@@ -322,8 +370,7 @@ void loop() {
   pppreviousaltitude = ppreviousaltitude;
   ppreviousaltitude = previousaltitude;
   previousaltitude = currentaltitude;
-  // Serial.println("flightstate: ");
-  // Serial.println(flightState);
+
 
   Wire1.requestFrom(TACHOMETER_ADRESS, 2);
   if (Wire1.available() == 2) {                // If 2 bytes are available
@@ -331,22 +378,21 @@ void loop() {
     byte highByte = Wire1.read();              // Read high byte
     uint16_t rpm = (highByte << 8) | lowByte;  // Combine the two bytes into a 16-bit integer
     // Print RPM value
-    Serial.println("******");
-    Serial.println(rpm);
-    Serial.println("******");
+    // Serial.println("******");
+    // Serial.println(rpm);
+    // Serial.println("******");
   } else {
     // Serial.println("incomplete data wire1");
   }
 
 
-  if (telemetry) {
+  if (telemetry){
     packet =
       ("3194,"                                                                                                 //TEAM_ID
-      + String(0)
-      //  + String(myGNSS.getHour()) + ":" + String(myGNSS.getMinute()) + ":" + String(myGNSS.getSecond()) + ","  //MISSION_TIME
-       + "0:00:00,"
+       + mission_time + ","
        + String(packetCount) + ","                                                                             //PACKET_COUNT
-       + "F,");                                                                                                  //MODE
+       + flightMode + ","
+      );                                                                                                  //MODE
       if (sentApo == false && (flightState == "DESCENT" || flightState == "APOGEE")){
         packet += "APOGEE";
         packet += ",";
@@ -356,11 +402,14 @@ void loop() {
         packet += ",";
       }
       packet += (                                                                       //STATE
-         String(float(bmp.readAltitude(calibrated_pressure)), 1) + ","                                         //ALTITUDE
+        //  String(float(bmp.readAltitude(calibrated_pressure)), 1) + ","                                         //ALTITUDE
+         String(currentaltitude) + ","
        + String(float(bmp.readTemperature()), 1) + ","                                                         //TEMPERATURE
        + String(float(bmp.readPressure() / 1000), 1) + ","                                                     //PRESSURE
-       + String(float((analogRead(VOLT_PIN)) * (3.3 / 4095.0) / (600.0 / 1985.5)), 1) + ","                    //VOLTAGE
-       + String(float(linearAccelData.acceleration.x)) + ","                                                   //GYRO X
+       + String(float((analogRead(VOLT_PIN)) * (3.3 / 4095.0) / (680.0 / 1500.0)) + 1.8, 1) + ",");                   //VOLTAGE
+      if (using_bno055){
+        packet += (
+         String(float(linearAccelData.acceleration.x)) + ","                                                   //GYRO X
        + String(float(linearAccelData.acceleration.y)) + ","                                                   //GYRO Y
        + String(float(linearAccelData.acceleration.z)) + ","                                                   //GYRO Z
        + String(float(accelerometerData.acceleration.x)) + ","
@@ -369,19 +418,24 @@ void loop() {
        + String(float(magnetometerData.magnetic.x / 100)) + ","
        + String(float(magnetometerData.magnetic.y / 100)) + ","
        + String(float(magnetometerData.magnetic.z / 100)) + ","
-       + String(rpm) + ","
-      //  + String(myGNSS.getHour()) + ":" + String(myGNSS.getMinute()) + ":" + String(myGNSS.getSecond()) + ","
-      + "00:00:00,"
-      //  + String(float(gps_altitude / 1000.0), 1) + ","
-      //  + String(float(latitude / 10000000.0), 4) + ","
-      //  + String(float(longitude / 10000000.0), 4) + ","
-      //  + String(SIV) + ","
-      + "0.0,0.0000,0.0000,0,"
-       + String(echo))
-      + ",,ON,00:00";
+        );
+      }
+      packet += (
+        String(rpm) + ","
+      + String(myGNSS.getHour()) + ":" + String(myGNSS.getMinute()) + ":" + String(myGNSS.getSecond()) + ","
+      + String(float(gps_altitude / 1000.0), 1) + ","
+      + String(float(latitude / 10000000.0), 4) + ","
+      + String(float(longitude / 10000000.0), 4) + ","
+      + String(SIV) + ","
+      + String(echo) + ","
+      + "ON,"
+      + cur_time()
+      );
 
 
   }
-  delay(1);
+  else{
+    packet = "";
+  }
   
 }
